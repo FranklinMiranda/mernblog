39ea6ff6ce036410f6fa1b72b9372c6730376491
FranklinMiranda <fdm61344@gmail.com>
2023-03-06 11:45:54 -0500

    CM

--- server/db/conn.js
+++ server/db/conn.js
@@ -8,15 +8,19 @@ const client = new MongoClient(Db, {
 var _db;
 
 module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
+  connectToServer:  (callback) => {
+    client.connect((err, db) => {
+      console.log("client.connect")
       // Verify we got a good "db" object
       if (db) {
         _db = db.db('employees');
         console.log('Successfully connected to MongoDB.');
       }
+      
       return callback(err);
-    });
+    })
+    
+    
   },
 
   getDb: function () {
