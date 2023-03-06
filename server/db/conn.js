e92c91b29c4ddcd4d68e130de573ad039406b1cc
FranklinMiranda <fdm61344@gmail.com>
2023-03-06 12:13:44 -0500

    CM

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,25 +1,29 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require('mongodb');
 const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
- 
+
 var _db;
- 
+
 module.exports = {
   connectToServer: function (callback) {
+
+    
+    console.log('connectToServer');
     client.connect(function (err, db) {
+      console.log('client.connect');
       // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("employees");
-        console.log("Successfully connected to MongoDB."); 
+      if (db) {
+        _db = db.db('employees');
+        console.log('Successfully connected to MongoDB.');
       }
+
       return callback(err);
-         });
+    });
   },
- 
+
   getDb: function () {
     return _db;
   },
